d532dc693f2db13c73d7ed45fc3a8424ac0b7635
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 12:03:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ def summon_captain_planet(planeteer_calls)
 end
 summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
 end
 
 def find_the_cheese# code an argument here
