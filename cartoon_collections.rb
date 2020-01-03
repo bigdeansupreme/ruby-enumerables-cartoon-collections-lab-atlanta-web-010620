0c92b12067915d0a420afe2a820aed18ba088205
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 12:01:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,10 @@ end
 roll_call_dwarves(dwarves)
 
 
-planeteer_calls = %w[earth wind fire water]
+planeteer_calls = %w[earth wind fire water heart]
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
