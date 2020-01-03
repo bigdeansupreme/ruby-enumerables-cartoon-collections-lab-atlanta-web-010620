ae2237909b65e6abc516589667848761183e0adb
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 12:06:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,8 @@ def long_planeteer_calls(planeteer_calls)
 end
 long_planeteer_calls(planeteer_calls)
 
+
+snacks = 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
