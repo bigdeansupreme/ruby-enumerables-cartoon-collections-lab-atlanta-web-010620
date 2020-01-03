f96bcb6c04fffb5528d0adc204f36a06647ded8a
Dino Kunovac <dinokunovac@gmail.com>
2020-01-03 12:04:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ summon_captain_planet(planeteer_calls)
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
+long_planeteer_calls
 
 def find_the_cheese# code an argument here
   # the array below is here to help
